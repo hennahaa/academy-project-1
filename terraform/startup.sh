1a316d76a8b3da5f056c07d13f4c46a9a561e491
Henna <henna.haapala@gmail.com>
2021-11-09 15:48:41 +0200

    startupin siivous jotta ei asenneta mitään turhaa

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -1,10 +1,1 @@
- #tämä saattaa joutua vaihtoon, jos nodea ei käytetäkään
- 
  #! /bin/bash
-sudo su -
-umask 077
-apt update
-apt -y install git-core curl build-essential openssl libssl-dev
-curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-sudo apt -y install nodejs
-npm install sendgrid
