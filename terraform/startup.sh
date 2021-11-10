fb0388b30d941c95c197976bc2d1ee727af1a19d
Henna <henna.haapala@gmail.com>
2021-11-10 19:07:21 +0200

    startup scriptin korjaus

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-sudo apt update && sudo apt upgrade 
-sudo apt install python3-pip
+gsutil cp gs://projekti_ampari/*.py .
+gsutil cp gs://projekti_ampari/.env .
+sudo apt update
+sudo apt -y upgrade
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.5 2
+sudo apt -y install python3-pip
 sudo pip3 install python-dotenv
-sudo pip3 install crontab
-gsutil cp gs://projekti_ampari/testi.txt . #kesken
+sudo pip3 install python-crontab
+python cron1.py
