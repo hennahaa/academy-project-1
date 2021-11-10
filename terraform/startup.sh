c995daaaa8c3a77ffb70ec30b4753eef19f81dba
jphieno <jari-pekka.heino@outlook.com>
2021-11-10 16:38:00 +0200

    created startup script to connect instance to sql

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -1,1 +1,5 @@
  #! /bin/bash
+sudo apt-get update
+sudo apt-get install postgresql-client
+export dbpass=`gcloud beta secrets versions access 1 --secret="projekti1"`
+psql "sslmode=disable dbname=tuntikirjaus user=postgres hostaddr=10.0.1.3 password=$dbpass"
