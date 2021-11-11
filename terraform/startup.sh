b7af8a452c3a11880df8b8d370d0e3e99fb2b13b
Henna <henna.haapala@gmail.com>
2021-11-11 12:28:14 +0200

    startup.sh versio jossa cronjob pyörii klo 00:00

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -2,10 +2,8 @@
 sudo gsutil cp gs://projekti_ampari/*.py .
 sudo gsutil cp gs://projekti_ampari/.env .
 sudo apt-get update
-sudo apt-get -y upgrade
 sudo apt-get -y install python3-pip
 sudo apt-get -y install postgresql-client
 sudo pip3 install python-dotenv
 sudo pip3 install psycopg2-binary
-line="* * * * * python3 /send_hours.py"
-(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
+(sudo crontab -u root -l; echo "0 0 * * * python3 /send_hours.py" ) | sudo crontab -u root -
